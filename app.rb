acc52246746809c7895c0b2482f746ef321d338e
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-09-11 15:33:49 +0100

    Kitty is on website

--- app.rb
+++ app.rb
@@ -12,3 +12,13 @@ end
 get '/public' do
   'AW17'
 end
+
+get '/cat' do
+  send_file 'cat.html'
+end
+
+get '/cat-raw-html' do
+"<div style= 'border: 3px dashed red'>
+<img src='http://bit.ly/1eze8aE'>
+</div>"
+end
